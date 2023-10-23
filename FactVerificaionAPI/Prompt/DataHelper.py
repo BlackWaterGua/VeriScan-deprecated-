519300db06943dc92a37c7bb73586e94107a40bb
DESKTOP-JJR84E9\Milton <stmilton2014@gmail.com>
2023-10-23 21:06:09 +0800

    everify完成

--- FactVerificaionAPI/Prompt/DataHelper.py
+++ FactVerificaionAPI/Prompt/DataHelper.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90118cf601a4dfe749ba5510c465bc2636d81b4e261b65ed7c55fc523a78cd22
-size 2202
+oid sha256:38e21955ea53d3407f1c31fed354416a6f2b4cfd115dbc7ba4309809ba27d6dd
+size 2351
--- FactVerificaionAPI/Prompt/Prompt.py
+++ FactVerificaionAPI/Prompt/Prompt.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:459062a20eb5c9bfbd328005dc60624134bfc30c81a44deb1465239e08e77ba9
-size 7474
+oid sha256:72ef63de4927abc62f474fd5ef5e701736b6a3f42702073acf804a446a0d6079
+size 7661
