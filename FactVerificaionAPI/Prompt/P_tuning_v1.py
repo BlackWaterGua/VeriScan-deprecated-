715c99513d8f74718594f5ec840a009cc9a7a329
DESKTOP-JJR84E9\Milton <stmilton2014@gmail.com>
2023-10-24 13:25:34 +0800

    fix lfs

--- FactVerificaionAPI/Prompt/P_tuning_v1.py
+++ FactVerificaionAPI/Prompt/P_tuning_v1.py
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d317a5c6e5047533ad62d6a540acc6179177eb0ab689eaefcee0c41bb14eaed6
-size 1300
+from Prompt.Prompt import Prompt
+from openprompt.prompts import PtuningTemplate, ManualVerbalizer
+from openprompt import PromptForClassification
+
+class P_tuning_v1(Prompt):
+    def __init__(self, model_name, checkpoint, inference=False):
+        super().__init__(model_name, checkpoint, inference)
+        self.template = PtuningTemplate(
+                            model=self.plm,
+                            tokenizer=self.tokenizer,
+                            text='{"placeholder":"text_a", "shortenable":True} {"soft":"according to the above content, the following description:"} {"placeholder":"text_b", "shortenable":True} {"soft":"whether it is correct? please answer, yes or no or maybe."} {"mask"}'
+                        )
+        self.verbalizer = ManualVerbalizer(
+                            classes=self.classes,
+                            num_classes=3,
+                            label_words=[["yes", "right"], ["no"], ["maybe"]],
+                            tokenizer=self.tokenizer,
+                        )
+        self.model = PromptForClassification(
+                            template=self.template,
+                            plm=self.plm,
+                            verbalizer=self.verbalizer,
+                            freeze_plm = False
+                        )
+        
--- FactVerificaionAPI/SentenceBert/SentenceBert.py
+++ FactVerificaionAPI/SentenceBert/SentenceBert.py
@@ -1,3 +1,57 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9f222a4d18140d4730f14cf4726b61c55146f190edb4a27355091637ffa36533
-size 1996
+import os
+import json
+import torch
+import numpy as np
+from tqdm import tqdm
+from torch import nn
+from model_cls import Model
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from transformers import AdamW, get_linear_schedule_with_warmup
+from transformers.models.bert import BertTokenizer
+from transformers import RobertaTokenizer, RobertaModel
+
+from data_helper import load_data, SentDataSet, collate_func, convert_token_id
+# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+def get_similar_sentence(claim, evidences, bert_pretrain_path, checkpoint_path):
+    tokenizer = BertTokenizer.from_pretrained(bert_pretrain_path)
+    model = Model(bert_pretrain_path)
+    loss_fct = nn.CrossEntropyLoss()
+    if torch.cuda.is_available():
+        model.cuda()
+        loss_fct.cuda()
+
+    # model.load_state_dict(torch.load(f"./FactVerificaionAPI/checkpoint/sentenceBert_bert_checkpoint.ckpt"))
+    model.load_state_dict(torch.load(checkpoint_path))
+
+    model.eval()
+
+    sent2sim = {}
+    for ev_sent in evidences:
+        sent2sim[ev_sent] = cosSimilarity(claim, ev_sent, model, tokenizer)
+    sent2sim = list(sent2sim.items())
+    sent2sim.sort(key=lambda s: s[1], reverse=True)
+
+    # ev_sent = [s[0] for s in sent2sim[:5] if s[1] > 0.8]
+    ev_sent_th08 = [s[0] for s in sent2sim if s[1] > 0.8]
+    # ev_sent_out5 = [s[0] for s in sent2sim[:5]]
+    return ev_sent_th08
+
+
+
+def cosSimilarity(sent1, sent2, model, tokenizer):
+    model.eval()
+    s1_input_ids, s1_mask, s1_segment_id = convert_token_id(sent1, tokenizer)
+    s2_input_ids, s2_mask, s2_segment_id = convert_token_id(sent2, tokenizer)
+
+    if torch.cuda.is_available():
+        s1_input_ids, s2_input_ids = s1_input_ids.cuda(), s2_input_ids.cuda()
+
+    with torch.no_grad():
+        s1_embeddings = model.encode(s1_input_ids, encoder_type='last-avg')
+        s2_embeddings = model.encode(s2_input_ids, encoder_type='last-avg')
+
+    cos_sim = F.cosine_similarity(s1_embeddings, s2_embeddings)
+    return cos_sim.item()
+
